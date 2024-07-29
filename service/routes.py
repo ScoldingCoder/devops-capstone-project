818b6f338565d977b030f28d7a4808522e355773
Caleb Beard <caleb_beard@hotmail.com>
2024-07-28 20:13:07 -0400

    add account deletion

--- service/routes.py
+++ service/routes.py
@@ -106,7 +106,20 @@ def read_account(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    """
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+        # use the Account.find() method to retrieve the account by the account_id
+        # if found, call the delete() method on the account
+    account = Account.find(account_id)
+    if account :
+        account.delete()
+
+    return"", status.HTTP_204_NO_CONTENT 
 
 
 ######################################################################
